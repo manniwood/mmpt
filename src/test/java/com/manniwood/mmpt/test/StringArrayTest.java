687f4513eb5acfd2741efe9041b3485bf9481602
Manni Wood <manni.lee.wood@gmail.com>
2014-05-23 21:59:31 -0400

    Adds String[] support; fixes Bintray settings
    Bintray settings will default to bogus settings
    of local gradle.properties when ${HOME}/.gradle/gradle.properties
    does not exist. This is better than the previous behaviour
    of the build failing because two properties were not defined.

---
+++ src/test/java/com/manniwood/mmpt/test/StringArrayTest.java
@@ -0,0 +1,96 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Manni Wood
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package com.manniwood.mmpt.test;
+
+import java.util.Arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.manniwood.mmpt.test.beans.StringArrayBean;
+
+@Test
+public class StringArrayTest extends TypeTest {
+
+    private static final String TABLE_CREATE_ID = "test.createStringArrayTestTable";
+
+    public StringArrayTest() {
+        super(TABLE_CREATE_ID);
+    }
+
+    @Test
+    public void testStringArray() {
+        String testName = "basic test";
+        StringArrayBean t = new StringArrayBean();
+        String[] intArray = new String[] { "one", "two", "three" };
+        t.setStringArray(intArray);
+        t.setName(testName);
+        session.insert("test.insertStringArray", t);
+        session.commit(true);
+
+        StringArrayBean result;
+        result = session.selectOne("test.selectStringArray", t);
+        session.rollback(true);  // just a select; rollback
+
+        Assert.assertTrue(Arrays.equals(intArray, result.getStringArray()), "String arrays need to match.");
+        Assert.assertEquals(testName, result.getName(), "Test name needs to be " + testName);
+    }
+
+    @Test
+    public void testStringArrayWithEmbeddedNulls() {
+        String testName = "embedded null test";
+        StringArrayBean t = new StringArrayBean();
+        String[] intArray = new String[] { "one", null, "three" };
+        t.setStringArray(intArray);
+        t.setName(testName);
+        session.insert("test.insertStringArray", t);
+        session.commit(true);
+
+        StringArrayBean result;
+        result = session.selectOne("test.selectStringArray", t);
+        session.rollback(true);  // just a select; rollback
+
+        Assert.assertTrue(Arrays.equals(intArray, result.getStringArray()), "String arrays need to match.");
+        Assert.assertEquals(testName, result.getName(), "Test name needs to be " + testName);
+    }
+
+    @Test
+    public void testNullStringArray() {
+        String testName = "null test";
+        StringArrayBean t = new StringArrayBean();
+        String[] intArray = null;
+        t.setStringArray(intArray);
+        t.setName(testName);
+        session.insert("test.insertStringArray", t);
+        session.commit(true);
+
+        StringArrayBean result;
+        result = session.selectOne("test.selectNullStringArray", t);
+        session.rollback(true);  // just a select; rollback
+
+        Assert.assertNull(result.getStringArray(), "String array must be null.");
+        Assert.assertEquals(testName, result.getName(), "Test name needs to be " + testName);
+    }
+}
+
